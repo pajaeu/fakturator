181ab2a00a7fafa3e4e6a6b7d22d86970fb87bd1
Paul <fgocsgo@gmail.com>
2025-05-14 15:58:49 +0200

    feat: add localisation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,18 @@
 <x-auth-layout>
-    <h1 class="mb-10 text-3xl font-semibold text-center">Vítejte zpátky</h1>
+    <h1 class="mb-10 text-3xl font-semibold text-center">{{ __('Welcome back') }}</h1>
     <form action="{{ route('login') }}" method="post">
         <div class="mb-4">
-            <x-form.input type="email" name="email" value="{{ old('email') }}" placeholder="Email" @class(['border-red-500' => $errors->has('email')])/>
+            <x-form.input type="email" name="email" value="{{ old('email') }}" placeholder="{{ __('Email') }}" @class(['border-red-500' => $errors->has('email')])/>
             <x-form.input-error name="email"/>
         </div>
         <div class="mb-5">
-            <x-form.input type="password" name="password" placeholder="Heslo" @class(['border-red-500' => $errors->has('password')])/>
+            <x-form.input type="password" name="password" placeholder="{{ __('Password') }}" @class(['border-red-500' => $errors->has('password')])/>
             <x-form.input-error name="password"/>
         </div>
         <div>
             @csrf
-            <button type="submit" class="cursor-pointer w-full py-3 px-4 rounded-full text-white font-medium bg-blue-700 hover:bg-blue-800 transition-colors">Přihlásit se</button>
+            <button type="submit" class="cursor-pointer w-full py-3 px-4 rounded-full text-white font-medium bg-blue-700 hover:bg-blue-800 transition-colors">{{ __('Log In') }}</button>
         </div>
-        <a href="{{ route('register') }}" class="block mt-3 text-center text-sm text-gray-500 underline hover:text-gray-600 transition-colors">Ještě nemáte účet? Zaregistrujte se.</a>
+        <a href="{{ route('register') }}" class="block mt-3 text-center text-sm text-gray-500 underline hover:text-gray-600 transition-colors">{{ __('No account yet? Register here.') }}</a>
     </form>
 </x-auth-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,27 @@
 <x-auth-layout>
-    <h1 class="mb-10 text-3xl font-semibold text-center">Nový účet</h1>
+    <h1 class="mb-10 text-3xl font-semibold text-center">{{ __('New account') }}</h1>
     <form action="{{ route('register') }}" method="post">
         <div class="mb-4">
-            <x-form.input type="email" name="email" value="{{ old('email') }}" placeholder="Email *" @class(['border-red-500' => $errors->has('email')])/>
+            <x-form.input type="email" name="email" value="{{ old('email') }}" placeholder="{{ __('Email') }} *" @class(['border-red-500' => $errors->has('email')])/>
             <x-form.input-error name="email"/>
         </div>
         <div class="mb-4">
-            <x-form.input type="text" name="company_id" value="{{ old('company_id') }}" placeholder="IČO" @class(['border-red-500' => $errors->has('company_id')])/>
+            <x-form.input type="text" name="company_id" value="{{ old('company_id') }}" placeholder="{{ __('Company ID') }}" @class(['border-red-500' => $errors->has('company_id')])/>
             <x-form.input-error name="company_id"/>
         </div>
         <div class="mb-5">
-            <x-form.input type="password" name="password" placeholder="Heslo *" @class(['border-red-500' => $errors->has('password')])/>
+            <x-form.input type="password" name="password" placeholder="{{ __('Password') }} *" @class(['border-red-500' => $errors->has('password')])/>
             <x-form.input-error name="password"/>
         </div>
         <div class="mb-5">
-            <x-form.input type="password" name="password_confirmation" placeholder="Potvrdit heslo *" @class(['border-red-500' => $errors->has('password_confirmation')])/>
+            <x-form.input type="password" name="password_confirmation" placeholder="{{ __('Confirm password') }} *" @class(['border-red-500' => $errors->has('password_confirmation')])/>
             <x-form.input-error name="password_confirmation"/>
         </div>
         <div>
             @csrf
-            <button type="submit" class="cursor-pointer w-full py-3 px-4 rounded-full text-white font-medium bg-blue-700 hover:bg-blue-800 transition-colors">Zaregistrovat se</button>
+            <button type="submit" class="cursor-pointer w-full py-3 px-4 rounded-full text-white font-medium bg-blue-700 hover:bg-blue-800 transition-colors">
+                {{ __('Register') }}</button>
         </div>
-        <a href="{{ route('login') }}" class="block mt-3 text-center text-sm text-gray-500 underline hover:text-gray-600 transition-colors">Již máte účet? Přihlašte se.</a>
+        <a href="{{ route('login') }}" class="block mt-3 text-center text-sm text-gray-500 underline hover:text-gray-600 transition-colors">{{ __('Have an account? Log In here.') }}</a>
     </form>
 </x-auth-layout>
