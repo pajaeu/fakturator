cf1c35fb20e54639f6cb6e2fc80d2f92a1efaeb0
Paul <fgocsgo@gmail.com>
2025-05-14 18:52:44 +0200

    feat: add migrations for invoices and contacts

---
+++ app/Livewire/Invoices/Create.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Invoices;
+
+use App\Livewire\Concerns\HasInvoiceItems;
+use Illuminate\View\View;
+use Livewire\Component;
+
+final class Create extends Component
+{
+    use HasInvoiceItems;
+
+    public function render(): View
+    {
+        return view('livewire.invoices.create');
+    }
+}
---
+++ database/migrations/2025_05_14_163039_create_invoices_table.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->index()->unique();
+            $table->date('billed_at');
+            $table->date('due_at');
+            $table->double('total');
+            $table->string('supplier_company');
+            $table->string('supplier_address');
+            $table->string('supplier_city');
+            $table->string('supplier_country');
+            $table->string('supplier_zip');
+            $table->string('customer_company');
+            $table->string('customer_company_id');
+            $table->string('customer_vat_id')->nullable();
+            $table->string('customer_address');
+            $table->string('customer_city');
+            $table->string('customer_country');
+            $table->string('customer_zip');
+            $table->string('customer_phone')->nullable();
+            $table->string('customer_email')->nullable();
+            $table->text('note')->nullable();
+            $table->foreignId('contact_id')->index()->nullable()->constrained('contacts')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
