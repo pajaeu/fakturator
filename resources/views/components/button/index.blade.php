d73c226f3146469fdfe43884c3bdf42f227f42b1
Paul <fgocsgo@gmail.com>
2025-05-16 18:11:39 +0200

    feat: add extra buttons to settings page

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -8,9 +8,10 @@
 @endphp
 
 <{{ $tag }} {{ $attributes->class([
-	'cursor-pointer flex items-center gap-2 py-2 px-4 rounded-full text-sm font-medium border transition-colors',
+	'cursor-pointer group flex items-center gap-2 py-2 px-4 rounded-full text-sm font-medium border transition-colors',
 	'text-white border-blue-600 bg-blue-600 hover:border-blue-700 hover:bg-blue-700' => $variant === 'primary',
 	'text-blue-600 border-blue-600 hover:text-white hover:bg-blue-600' => $variant === 'outline',
+	'text-gray-600 border-gray-200 hover:text-gray-800 hover:border-gray-300 hover:bg-gray-100' => $variant === 'outline-gray',
 ]) }}>
     {{ $slot }}
 </{{ $tag }}>
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,17 +1,36 @@
 <x-app-layout>
     <x-card class="mb-5 p-10">
-        <div class="flex justify-center items-center gap-5 lg:gap-10">
-            <img src="{{ auth()->user()->avatarUrl() }}" alt="Avatar" class="size-20 lg:size-24 rounded-full">
-            <div>
-                <div class="text-2xl font-medium">{{ auth()->user()->billing_company }}</div>
-                <div class="mb-4 text-gray-500">{{ auth()->user()->email }}</div>
-                <div class="flex items-center gap-3">
-                    @foreach(config('app.available_locales') as $locale)
-                        <a href="{{ route('locale.switch', ['locale' => $locale]) }}" wire:navigate class="size-7 rounded-full overflow-hidden @if(app()->getLocale() !== $locale) opacity-30 hover:opacity-80 @endif transition-all">
-                            <img src="{{ asset("assets/images/locales/{$locale}.png") }}" alt="{{ $locale }}" class="size-full">
-                        </a>
-                    @endforeach
+        <div class="flex items-center">
+            <div class="flex items-center gap-5 lg:gap-10">
+                <img src="{{ auth()->user()->avatarUrl() }}" alt="Avatar" class="size-20 lg:size-24 rounded-full">
+                <div>
+                    <div class="text-2xl font-medium">{{ auth()->user()->billing_company }}</div>
+                    <div class="mb-4 text-gray-500">{{ auth()->user()->email }}</div>
+                    <div class="flex items-center gap-3">
+                        @foreach(config('app.available_locales') as $locale)
+                            <a href="{{ route('locale.switch', ['locale' => $locale]) }}" wire:navigate class="size-7 rounded-full overflow-hidden @if(app()->getLocale() !== $locale) opacity-30 hover:opacity-80 @endif transition-all">
+                                <img src="{{ asset("assets/images/locales/{$locale}.png") }}" alt="{{ $locale }}" class="size-full">
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
+            </div>
+            <div class="ms-auto flex items-center gap-3">
+                <x-button variant="outline-gray">
+                    <x-icons.pencil class="size-6 text-blue-600"/>
+                    <span>{{ __('Edit account') }}</span>
+                </x-button>
+                <x-button variant="outline-gray">
+                    <x-icons.lock class="size-6 text-blue-600"/>
+                    <span>{{ __('Change login credentials') }}</span>
+                </x-button>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <x-button type="submit" variant="outline-gray">
+                        <x-icons.logout class="size-6 text-blue-600"/>
+                        <span>{{ __('Log Out') }}</span>
+                    </x-button>
+                </form>
             </div>
         </div>
     </x-card>
