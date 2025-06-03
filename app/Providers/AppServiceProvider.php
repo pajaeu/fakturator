ccfa7085ba4f12d87812f8c7fd48dbc4a4b39ee0
Paul <fgocsgo@gmail.com>
2025-06-03 11:57:45 +0200

    feat: add strict models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -46,6 +46,8 @@ final class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
 
+        Model::shouldBeStrict();
+
         Model::automaticallyEagerLoadRelationships();
     }
 
