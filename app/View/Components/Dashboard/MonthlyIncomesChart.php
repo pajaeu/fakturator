20564ac4e489cdb15dcba270cc9031d435517acf
Paul <fgocsgo@gmail.com>
2025-06-04 15:38:07 +0200

    feat: cache monthly incomes

--- app/View/Components/Dashboard/MonthlyIncomesChart.php
+++ app/View/Components/Dashboard/MonthlyIncomesChart.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\View\Components\Dashboard;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\View\Component;
@@ -16,20 +17,22 @@ final class MonthlyIncomesChart extends Component
      */
     public function render(): View
     {
-        $data = DB::table('invoices')->selectRaw('strftime("%m", issued_at) as month, SUM(total_with_vat) as total')
-            ->whereYear('issued_at', now()->year)
-            ->groupBy('month')
-            ->orderBy('month')
-            ->pluck('total', 'month');
-
         $labels = collect(range(1, 12))->map(function ($monthNumber) {
             return Str::ucfirst(__(now()->startOfYear()->month((int) $monthNumber)->translatedFormat('F')));
         });
 
-        $data = collect(range(1, 12))->map(function ($month) use ($data) {
-            $monthKey = mb_str_pad((string) $month, 2, '0', STR_PAD_LEFT);
+        $data = Cache::remember('monthly-incomes-'.auth()->id(), 3600, function () {
+            $data = DB::table('invoices')->selectRaw('strftime("%m", issued_at) as month, SUM(total_with_vat) as total')
+                ->whereYear('issued_at', now()->year)
+                ->groupBy('month')
+                ->orderBy('month')
+                ->pluck('total', 'month');
 
-            return $data[$monthKey] ?? 0;
+            return collect(range(1, 12))->map(function ($month) use ($data) {
+                $monthKey = mb_str_pad((string) $month, 2, '0', STR_PAD_LEFT);
+
+                return $data[$monthKey] ?? 0;
+            });
         });
 
         return view('components.dashboard.monthly-incomes-chart', [
