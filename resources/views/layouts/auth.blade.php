ff3682f70c29861baa1c88295947dfb9901324e2
Paul <fgocsgo@gmail.com>
2025-06-05 14:00:45 +0200

    feat: add link back to landing on auth layout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,9 @@
     <div class="w-screen h-screen flex justify-center items-center">
         <div class="w-full max-w-[295px]">
             <div class="mb-7">
-                <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" class="mx-auto h-5">
+                <a href="{{ route('landing') }}">
+                    <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" class="mx-auto h-5">
+                </a>
             </div>
             <div>{{ $slot }}</div>
         </div>
