62517a8af2fd73014ba74dfc5cc7911a0937b9bc
Paul <fgocsgo@gmail.com>
2025-07-04 22:37:24 +0200

    feat: add current user scope to folder model

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Models\Scopes\CurrentUserScope;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -20,6 +22,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property int $user_id
  * @property-read User $user
  */
+#[ScopedBy(CurrentUserScope::class)]
 final class Folder extends Model
 {
     /** @return HasMany<Invoice, $this> */
